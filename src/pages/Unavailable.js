5b37b6950fe11503462f76e915a1844a1157f986
Tristan <tristanhancock@gmail.com>
2024-06-02 13:17:20 +0530

    UI updates+productdetails route

--- src/pages/Unavailable.js
+++ src/pages/Unavailable.js
@@ -4,7 +4,7 @@ import styles from '../styles/Unavailable.module.css';
 const Unavailable = () => {
     return (
         <div className={styles.container}>
-            <h1>Available Soon</h1>
+            <h1>Coming Soon</h1>
         </div>
     );
 };
